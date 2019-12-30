cb187e5b704b895871ee68a76f5177073a0476e6
Michael Hladky <michael@hladky.at>
2019-12-30 02:54:36 +0100

    fix(integration-helper): delete @ts-lint comments

--- packages/integration-helper/src/consumer.utils.ts
+++ packages/integration-helper/src/consumer.utils.ts
@@ -1,8 +1,6 @@
 export function setupGlobalCompilerOptions(compilerOptions: any): void {
   (window as any).globalCompilerOptions = {};
   const globalCompilerOptions = (window as any).globalCompilerOptions;
-  // tslint:disable-next-line:no-unused-expression
   compilerOptions.ngZone && (globalCompilerOptions.ngZone = compilerOptions.ngZone);
-  // tslint:disable-next-line:no-unused-expression
   compilerOptions.injector && (globalCompilerOptions.injector = compilerOptions.injector);
 }
