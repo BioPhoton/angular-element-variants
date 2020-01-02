de7abc80bea6b5c6c91144d28b645e56256ac398
Michael Hladky <michael@hladky.at>
2020-01-02 02:22:53 +0100

    delete

--- schematics-repo/schematics-repo/apps/schematics-repo-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('schematics-repo', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to schematics-repo!');
-  });
-});
--- schematics-repo/schematics-repo/apps/schematics-repo/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'schematics-repo',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/schematics-repo',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
